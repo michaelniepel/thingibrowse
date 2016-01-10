6c39d73fbbaac9395398ac42175ac5e0ca0562f4
Michael Niepel <michael.niepel@gmail.com>
2016-01-10 23:32:02 +0100

    check response status

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -15,10 +15,21 @@ function receivedDirs() {
 	}
 }
 
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
 export function fetchDirs(splat) {
 	return dispatch => {
 		dispatch(requestDirs())
 		return fetch('/api/models/'+(splat !== undefined ? splat : ''))
+			.then(checkStatus)
 			.then(resp => resp.json())
 			.then(json => dispatch({
 				type: RECEIVED_DIRS,
